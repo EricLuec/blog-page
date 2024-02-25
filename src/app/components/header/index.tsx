edbcf8eab736cee4952aa21a86f264bc1cca7b40
ericluec <eric.luecking@gmail.com>
2024-02-25 17:35:09 +0100

    working on header

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+
+export default function Header(){
+
+    return <div>
+
+        <header className={`top-0 left-0 flex w-full items-cener bg-transparent`}>
+
+        </header>
+    </div>
+}
